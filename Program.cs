4ee0d28c6e191f86774b2992ed25ac7078e4a494
Guilherme Torres <torrescorreag@gmail.com>
2023-04-06 20:13:11 -0300

    Utilizando a Classe e Método
    Aprendendo a como utilizar Classes e Métodos para deixar o código principal mais limpo.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace exemploDeClasse
+{ 
+    class Program
+    {
+        // Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas válidas).//
+        // Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos possui a maior área//
+        // A fórmula para calcular a área de um triângulo a partir das medidas de seus lados a, b e c é a seguinte(fórmula de Heron)://
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triangulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triangulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+
+            double areaY = y.Area();
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exemploDeClasse
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area ()
+        {
+            double p = (A + B + C) / 2.0;
+            double raiz = Math.Sqrt (p * (p - A) * (p - B) * (p - C));
+            return raiz;
+        }
+    }
+}
